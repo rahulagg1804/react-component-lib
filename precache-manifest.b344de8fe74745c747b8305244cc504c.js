f28001056b30451113c0822c92ad7a2f7bbc55e8
rahulagg1804 <rahul.agg1804@github.com>
2020-04-29 00:35:19 +0530

    Updates

---
+++ precache-manifest.b344de8fe74745c747b8305244cc504c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ede7cef4e7a80da609a372e26c7143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "479845e3ce30eebf9cc4",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "22a6980c1bc5684eb3c9",
+    "url": "/static/js/2.aeeaf4c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aeeaf4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "479845e3ce30eebf9cc4",
+    "url": "/static/js/main.ab9e9146.chunk.js"
+  },
+  {
+    "revision": "52e4cdf748d8b3d1cd4e",
+    "url": "/static/js/runtime-main.9fe15c4c.js"
+  }
+]);
